e257c2250cfe10dd98e876d68825c220ff9e5f4f
shin nagai <shnnagai@gmail.com>
2021-06-12 12:55:24 +0900

    fix : Docker build issue

--- src/main/java/com/javareactive/javareactive/controller/HealthcheckController.java
+++ src/main/java/com/javareactive/javareactive/controller/HealthcheckController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthcheckController {
-
     @GetMapping("/health")
     public String healthcheck(){
         return "healthy";
